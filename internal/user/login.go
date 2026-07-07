47b4764f9da6078c3a1f09e573855eed0c8c4a8c
d2school <nanyu@sina.com>
2026-07-07 19:28:59 +0800

    refactor: 将SMS验证码日志从Info降级为Debug，去掉emoji符号

--- internal/user/login.go
+++ internal/user/login.go
@@ -53,7 +53,7 @@ func (h *Handler) OnRequestVerifyCode(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	h.logger.Infof("📱 SMS verify code for %s: %s (valid for 5 minutes)", req.Tel, verifyCode)
+	h.logger.Debugf("SMS verify code for %s: %s (valid for 5 minutes)", req.Tel, verifyCode)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]interface{}{
