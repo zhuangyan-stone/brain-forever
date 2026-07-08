bd21152c55f3b177ff71f090ff33529a98cc62ca
zhuangyan <zhuangyan-stone@hotmail.com>
2026-07-08 19:18:07 +0800

    创建数据库示例用户

---
+++ doc/sql/create_user_dev.sql
@@ -0,0 +1,18 @@
+CREATE USER 'dev'@'%' IDENTIFIED BY 'dev123456~PWD';
+GRANT ALTER ON d2brain.* TO 'dev'@'%';
+GRANT CREATE ON d2brain.* TO 'dev'@'%';
+GRANT CREATE VIEW ON d2brain.* TO 'dev'@'%';
+GRANT DELETE ON d2brain.* TO 'dev'@'%';
+GRANT DROP ON d2brain.* TO 'dev'@'%' WITH GRANT OPTION;
+GRANT INDEX ON d2brain.* TO 'dev'@'%';
+GRANT INSERT ON d2brain.* TO 'dev'@'%';
+GRANT REFERENCES ON d2brain.* TO 'dev'@'%';
+GRANT SELECT ON d2brain.* TO 'dev'@'%';
+GRANT SHOW VIEW ON d2brain.* TO 'dev'@'%';
+GRANT TRIGGER ON d2brain.* TO 'dev'@'%';
+GRANT UPDATE ON d2brain.* TO 'dev'@'%';
+GRANT CREATE TEMPORARY TABLES ON d2brain.* TO 'dev'@'%';
+GRANT LOCK TABLES ON d2brain.* TO 'dev'@'%' WITH GRANT OPTION;
+
+
+
