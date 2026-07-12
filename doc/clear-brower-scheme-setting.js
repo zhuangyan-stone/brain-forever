38507f0f14cb33a6196efb2a580ba791a6653cab
d2school <nanyu@sina.com>
2026-07-12 20:23:16 +0800

    清前端主题设置

---
+++ doc/clear-brower-scheme-setting.js
@@ -0,0 +1,3 @@
+localStorage.removeItem('brainforever_theme_light');   // 清亮色主题
+localStorage.removeItem('brainforever_theme_dark');     // 清暗色主题
+localStorage.removeItem('brainforever_settings');       // 清模式等所有设置
